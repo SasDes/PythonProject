cfc6e5d85a25afbf9401177a5a1d3f5118a0fa25
SasDes <139076648+SasDes@users.noreply.github.com>
2024-01-17 16:12:36 +0100

    Update DV0101EN-Final_Assign_Part_2_Questions_240117.py

--- DV0101EN-Final_Assign_Part_2_Questions_240117.py
+++ DV0101EN-Final_Assign_Part_2_Questions_240117.py
@@ -25,7 +25,7 @@ app = dash.Dash(__name__)
 # Create the dropdown menu options
 dropdown_options = [
     {'label': 'Yearly Statistics', 'value': 'Yearly Statistics'},
-    {'label': 'Recession Period Statistics', 'value': 'Recession Statistics'}
+    {'label': 'Recession Period Statistics', 'value': 'Recession Period Statistics'}
 ]
 # List of years 
 year_list = [i for i in range(1980, 2024, 1)]
@@ -69,7 +69,7 @@ def update_input_container(selected_statistics):
 # Define the callback function to update the input container based on the selected statistics
 @app.callback(
     Output(component_id='output-container', component_property='children'),
-    [Input(component_id='select-year', component_property='value'), Input(component_id='dropdown-statistics', component_property='value')])
+    [Input(component_id='dropdown-statistics', component_property='value'), Input(component_id='select-year', component_property='value')])
 
 
 def update_output_container(selected_statistics, input_year):
@@ -150,7 +150,7 @@ def update_output_container(selected_statistics, input_year):
         )
         
 # Plot 2 Total Monthly Automobile sales using line chart.
-        mas = data.groupby('Month')['Automobile_sales'].mean().reset_index()
+        mas = data.groupby('Month')['Automobile_Sales'].mean().reset_index()
         Y_chart2 = dcc.Graph(figure=px.line(mas,
             x='Month',
             y='Automobile_Sales',
@@ -168,12 +168,12 @@ def update_output_container(selected_statistics, input_year):
             )
 
             # Total Advertisement Expenditure for each vehicle using pie chart
-        exp_data=yearly_data.groupby('Vehicle_Type')['Advertising_Expenditure'].sum().reset_index()
+        exp_data=yearly_data.groupby('Advertising_Expenditure')['Vehicle_Type'].sum().reset_index()
         Y_chart4 = dcc.Graph(figure=px.pie(
             exp_data,
             values='Advertising_Expenditure',
-            names=Vehicle_Type,
-            titel="Advertising Expenditure by Vehicle Type over year")
+            name='Vehicle_Type',
+            title="Advertising Expenditure by Vehicle Type over year")
             )
                 
 #TASK 2.6 yearlyData: Returning the graphs for displaying Yearly data
