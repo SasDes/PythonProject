26ea563c5fc24936daa9f9db1d7d0f87f66fbb1d
SasDes <139076648+SasDes@users.noreply.github.com>
2024-01-17 16:20:59 +0100

    Update DV0101EN-Final_Assign_Part_2_Questions_240117.py

--- DV0101EN-Final_Assign_Part_2_Questions_240117.py
+++ DV0101EN-Final_Assign_Part_2_Questions_240117.py
@@ -169,17 +169,17 @@ def update_output_container(selected_statistics, input_year):
 
             # Total Advertisement Expenditure for each vehicle using pie chart
         exp_data=yearly_data.groupby('Advertising_Expenditure')['Vehicle_Type'].sum().reset_index()
-        Y_chart4 = dcc.Graph(figure=px.pie(
-            exp_data,
+        Y_chart4 = dcc.Graph(figure=px.pie(exp_data,
             values='Advertising_Expenditure',
-            name='Vehicle_Type',
-            title="Advertising Expenditure by Vehicle Type over year")
+            names='Vehicle_Type',
+            title="Advertising Expenditure by Vehicle Type over year"
+            )
             )
                 
 #TASK 2.6 yearlyData: Returning the graphs for displaying Yearly data
         return [
             html.Div(className='chart-item', children=[html.Div(children=Y_chart1),html.Div(children=Y_chart2)],style={'display': 'flex'}),
-            html.Div(className='chart-item', children=[html.Div(children=Y_chart3),html.Div(children=y_chart4)],style={'display': 'flex'})
+            html.Div(className='chart-item', children=[html.Div(children=Y_chart3),html.Div(children=Y_chart4)],style={'display': 'flex'})
             ]
     else:
         return None
